d84baccc10f67796fd42fc634f4fc38dec6d3400
André <andre.l.santos7@docente.senai.br>
2025-05-23 19:49:13 -0300

    Novo Programa

--- teste.py
+++ teste.py
@@ -1,11 +1,1 @@
-num = int(input("Numero"))
-primo = True
-
-for x in range(2, num-1):
-    if (num % x == 0):
-        primo = False
-
-if (primo)
-    print("Primo")
-else:
-    print("Não Primo")
+print("Vai Corinthians")
